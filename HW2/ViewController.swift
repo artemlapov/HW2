efba4ba66a6e1e07f84360ff223accb1fad64bde
artemlapov <artemlapov@me.com>
2022-09-18 10:55:20 +0200

    Done!

--- HW2/ViewController.swift
+++ HW2/ViewController.swift
@@ -7,12 +7,13 @@
 
 import UIKit
 
+var numberOfClicks = 0
+
 class ViewController: UIViewController {
 
     @IBOutlet var redLightView: UIView!
     @IBOutlet var yellowLightView: UIView!
     @IBOutlet var greenLightView: UIView!
-
     @IBOutlet var switchLightButton: UIButton!
 
     override func viewDidLoad() {
@@ -21,11 +22,39 @@ class ViewController: UIViewController {
         yellowLightView.layer.cornerRadius = 65
         greenLightView.layer.cornerRadius = 65
 
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+
         switchLightButton.layer.cornerRadius = 15
+
     }
 
     @IBAction func switchLight() {
+        if numberOfClicks == 0 {
+            redLightView.alpha = 1
+            numberOfClicks += 1
+        } else {
+            switchLightButton.setTitle("NEXT", for: .normal)
+            switch numberOfClicks % 3 {
+            case 0:
+                redLightView.alpha = 1
+                greenLightView.alpha = 0.3
+                numberOfClicks += 1
+            case 1:
+                redLightView.alpha = 0.3
+                yellowLightView.alpha = 1
+                numberOfClicks += 1
+            case 2:
+                yellowLightView.alpha = 0.3
+                greenLightView.alpha = 1
+                numberOfClicks += 1
+            default:
+                break
+            }
+        }
     }
-
 }
+
+
 
